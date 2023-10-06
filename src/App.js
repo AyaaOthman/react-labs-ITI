47efd9c65c2c7a244fc15e916e8f00e4161f8f13
Aya Othman <ayaaothman96@gmail.com>
2023-10-06 22:17:07 +0300

    Day one Task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Component } from "react";
-import Home from "./components/Home/Home";
+import Parent from "./components/Parent/Parent";
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from the App Js</h1>
-        <Home />
+      <div className="container">
+        <Parent />
       </div>
     );
   }
--- src/components/Child/Child.jsx
+++ src/components/Child/Child.jsx
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 
 export default class Child extends Component {
   render() {
-    return <div>Child</div>;
+    let { name, price, model, id } = this.props.products;
+    return (
+      <div className="col-md-4">
+        <div className="bg-info p-3 rounded-2">
+          <h4> {name}</h4>
+          <p>product price: {price}</p>
+          <p>product model: {model}</p>
+          <div className="d-flex justify-content-between align-items-center">
+            <button
+              className="btn btn-dark"
+              onClick={() => this.props.updatePriceHandler(this.props.index)}
+            >
+              Increase Price
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => this.props.deleteProductFunction(id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
